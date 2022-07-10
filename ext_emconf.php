26d15585bd104b666d10aafe0cbbaf931de17edc
Renzo Bauen <info@conpassione.ch>
2022-07-10 18:12:24 +0200

    Created ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ #---------------------------------------------------------------
+ # ext_tables file for ext sitepackage
+ #
+ # Generated 01.07.2017 by www.conpassione.ch
+ #---------------------------------------------------------------
+ */
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'SSWBZI',
+	'description' => 'Custom Layout Template by conPassione gmbh',
+	'category' => 'templates',
+	'version' => '11.0',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Renzo Bauen',
+	'author_email' => 'info@conpassione.ch',
+	'author_company' => 'conPassione gmbh',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'php' => '8.1.0-0.0.0',
+			'typo3' => '11.5.0-0.0.0',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+);
+?>
